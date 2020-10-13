0d20f8af3d0e07710030fca7c97480970c894b9c
Oskar <leoskar@student.chalmers.se>
2020-10-13 13:57:50 +0200

    Added gaussian driver

--- vqe_wrapper/VQE_wrapper.py
+++ vqe_wrapper/VQE_wrapper.py
@@ -49,24 +49,26 @@ class VQEWrapper():
         self.freeze_core = True
         self.orbital_reduction = []
 
-        #Parallelization threshold in number of qubits (14 if not specified)
-        self.par_thres = 14
-
         self.qubit_op = None
         self.aux_ops = None
         self.initial_point = None
 
         self.optimizer = SLSQP(maxiter=5000)
 
-        # Choose the backend (use Aer instead of BasicAer) 
-        self.backend = Aer.get_backend('statevector_simulator') 
-        self.quantum_instance = QuantumInstance(backend=self.backend,
-                                                backend_options = {'statevector_parallel_threshold':self.par_thres})
-
         self.vqe_algo = None
 
         self.vqe_callback = None
         self.vqe_time = None
+
+        #Choose the backend (use Aer instead of BasicAer) 
+        self.simulator = 'statevector_simulator'
+        self.backend_options = {}
+
+
+    def init_backend(self):
+        self.backend = Aer.get_backend(self.simulator) 
+        self.quantum_instance = QuantumInstance(backend=self.backend,
+                                                backend_options = self.backend_options)
 
     def opt_str(self):
         match = re.search(r'optimizers.[A-z]+.(.+) object', str(self.optimizer))
@@ -74,16 +76,12 @@ class VQEWrapper():
         return opt_str
 
     def gaussian_config(self):
-        #Formats properties to a string fitting the gaussian input format
+        #Format properties to a string fitting the gaussian input format
         gaussian_config = f'# {self.hf_method.value}/{self.basis} scf(conventional)\n\nMolecule\n\n{self.charge} {self.spin+1}\n'
         gaussian_config = gaussian_config + self.molecule_string.replace('; ','\n') + '\n\n'
         return gaussian_config
 
     def initiate(self):
-        #print("Setting up system:")
-        #print(f"  Molecule: {self.molecule_string}")
-        #print(f"  Charge: {self.charge}")
-        #print(f"  Spin: {self.spin}")
         if self.chem_driver.value == 'PySCF':
             self.driver = PySCFDriver(atom=self.molecule_string, 
                                       unit=self.length_unit, 
@@ -98,20 +96,20 @@ class VQEWrapper():
         self.qmolecule = self.driver.run()
 
         self.core = Hamiltonian(transformation=self.transformation, 
-                           qubit_mapping=self.qubit_mapping, 
-                           two_qubit_reduction=self.two_qubit_reduction, 
-                           freeze_core=self.freeze_core, 
-                           orbital_reduction=self.orbital_reduction)
+                                qubit_mapping=self.qubit_mapping, 
+                                two_qubit_reduction=self.two_qubit_reduction, 
+                                freeze_core=self.freeze_core, 
+                                orbital_reduction=self.orbital_reduction)
 
         self.qubit_op, self.aux_ops = self.core.run(self.qmolecule)
 
-        # initial state
+        #Initial state
         self.init_state = HartreeFock(num_orbitals=self.core._molecule_info['num_orbitals'], 
                                       qubit_mapping=self.core._qubit_mapping,
                                       two_qubit_reduction=self.core._two_qubit_reduction, 
                                       num_particles=self.core._molecule_info['num_particles'])
 
-        # UCCSD Ansatz
+        #UCCSD Ansatz
         self.var_form = UCCSD(num_orbitals=self.core._molecule_info['num_orbitals'], 
                               num_particles=self.core._molecule_info['num_particles'], 
                               initial_state=self.init_state, 
@@ -122,7 +120,7 @@ class VQEWrapper():
         
         self.init_vqe()
 
-    #set up VQE
+    #Set up VQE
     def init_vqe(self):
         self.vqe_algo = VQE(self.qubit_op, 
                             self.var_form, 
@@ -132,12 +130,12 @@ class VQEWrapper():
 
 
     def run_vqe(self):
-        # run the algorithm
+        #Run the algorithm
         vqe_start = timer()
         self.vqe_result = self.vqe_algo.run(self.quantum_instance)
         self.vqe_time = timer() - vqe_start
 
-        # get the results
+        #Get the results
         result = self.core.process_algorithm_result(self.vqe_result) 
 
         return result
